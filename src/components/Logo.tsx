60c1f78b09f1d6454ccff6a3b7dd74f1341a0a7f
Yelisieiev Kim <rbv271@outlook.com>
2024-01-17 14:08:05 +0200

    fixes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { ReactComponent as LogoIcon } from '../assets/img/icons/logo.svg';
 
 export const Logo = () => (
-  <a href="#/" aria-label="home" className="logo">
+  <a href="#" aria-label="home" className="logo">
     <LogoIcon />
     <LogoIcon className="logo__hex" />
   </a>
