3b06e87811d41b23afd20a477e2b89d3ad5b605c
Kim Yelisieiev <61588991+Yeliseiev@users.noreply.github.com>
2024-01-17 12:07:23 +0200

    Update projects.ts

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -4,7 +4,7 @@ export function wait(delay: number) {
   });
 }
 
-const URL = 'api/projects.json';
+const URL = 'public/api/projects.json';
 
 export const fetchProjects = async () => {
   const response = await fetch(URL);
